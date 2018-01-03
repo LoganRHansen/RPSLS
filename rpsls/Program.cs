91540f9686a421702f50884bd84b4354437fcfc4
Logan <lrh1443@gmail.com>
2018-01-03 07:56:03 -0600

    all vars defined

--- rpsls/Program.cs
+++ rpsls/Program.cs
@@ -11,12 +11,33 @@ namespace rpsls
         static void Main(string[] args)
         {
             string inputPlayer, inputComp;
-            int randomInt;
+            int randomNum;
 
             Console.WriteLine("Choose rock, paper, scissors, lizard, or spock")
                 inputPlayer = Console.ReadLine();
 
             Random random = new Random();
+            randomNum = random.Next(1, 6);
+            switch (randomNum)
+            {
+                case 1:
+                    inputComp = "rock";
+                    break;
+                case 2:
+                    inputComp = "paper";
+                    break;
+                case 3:
+                    inputComp = "scissors";
+                    break;
+                case 4:
+                    inputComp = "lizard";
+                    break;
+                case 5:
+                    inputComp = "spock";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
